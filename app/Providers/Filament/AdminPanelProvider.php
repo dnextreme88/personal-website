09ecef6e6faea5d8a05ac33a79f847dad38be613
Kevin Decena <storm_iz_here@yahoo.com>
2025-01-10 04:24:43 +0800

    add maxContentWidth to maximize admin panel width

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -10,6 +10,7 @@ use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
+use Filament\Support\Enums\MaxWidth;
 use Filament\Widgets;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
@@ -54,6 +55,7 @@ class AdminPanelProvider extends PanelProvider
             ->authMiddleware([
                 Authenticate::class,
             ])
+            ->maxContentWidth(MaxWidth::Full)
             ->viteTheme('resources/css/filament/admin/tailwind-theme.css');
     }
 }
