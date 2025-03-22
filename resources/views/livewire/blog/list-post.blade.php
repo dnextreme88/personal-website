f0220d8228238ec89f9a2c62cf4ac33b2f434089
Kevin Decena <storm_iz_here@yahoo.com>
2025-03-23 02:39:00 +0800

    add loader while loading posts

--- resources/views/livewire/blog/list-post.blade.php
+++ resources/views/livewire/blog/list-post.blade.php
@@ -28,37 +28,50 @@
         </form>
     @endif
 
-    @if ($is_filtered && $posts->total() >= 1)
-        <p class="text-green-800 dark:text-green-200">Your search returned {{ $posts->total() }} {{ $posts->total() == 1 ? 'result' : 'results' }}.</p>
-    @endif
+    <div wire:loading.flex>
+        <x-loading-indicator
+            :loader_color_bg="'fill-gray-200'"
+            :loader_color_spin="'fill-gray-200'"
+            :showText="true"
+            :size="4"
+            :text="'Loading posts'"
+            :text_color="'text-white'"
+        />
+    </div>
 
-    @forelse ($posts as $post)
-        <article class="flex max-w-2xl flex-col items-start justify-between">
-            <div class="flex items-center gap-x-2">
-                <time class="text-sm text-gray-500 dark:text-gray-400" datetime="{{ $post->date_published }}">{{ Carbon\Carbon::parse($post->date_published)->format('F j, Y') }}</time>
+    <div wire:loading.remove class="space-y-8">
+        @if ($is_filtered && $posts->total() >= 1)
+            <p class="mb-4 text-green-800 dark:text-green-200">Your search returned {{ $posts->total() }} {{ $posts->total() == 1 ? 'result' : 'results' }}.</p>
+        @endif
 
-                <span class="text-sm text-gray-800 dark:text-gray-200">/</span>
+        @forelse ($posts as $post)
+            <article class="flex max-w-2xl flex-col items-start justify-between">
+                <div class="flex items-center gap-x-2">
+                    <time class="text-sm text-gray-500 dark:text-gray-400" datetime="{{ $post->date_published }}">{{ Carbon\Carbon::parse($post->date_published)->format('F j, Y') }}</time>
 
-                <span class="text-xs relative rounded-full bg-gray-200 border border-gray-300 dark:bg-gray-700 px-3 py-1 font-medium text-gray-800 dark:text-gray-200">{{ $post->category->name }}</span>
-            </div>
+                    <span class="text-sm text-gray-800 dark:text-gray-200">/</span>
 
-            <div class="relative">
-                <h2 class="mt-3 text-2xl font-semibold text-gray-800 dark:text-gray-200 hover:text-blue-800 dark:hover:text-blue-200 transition duration-200">
-                    <a wire:navigate href="{{ route('blog.post.detail', ['id' => $post->id, 'slug' => $post->slug]) }}">{{ $post->title }}</a>
-                </h2>
+                    <span class="text-xs relative rounded-full bg-gray-200 border border-gray-300 dark:bg-gray-700 px-3 py-1 font-medium text-gray-800 dark:text-gray-200">{{ $post->category->name }}</span>
+                </div>
 
-                @if (str_word_count($post->description) > 80)
-                    <div class="mt-3 indent-3 text-justify line-clamp-5 text-gray-800 dark:text-gray-200">{!! Markdown::parse($post->description) !!}</div>
+                <div class="relative">
+                    <h2 class="mt-3 text-2xl font-semibold text-gray-800 dark:text-gray-200 hover:text-blue-800 dark:hover:text-blue-200 transition duration-200">
+                        <a wire:navigate href="{{ route('blog.post.detail', ['id' => $post->id, 'slug' => $post->slug]) }}">{{ $post->title }}</a>
+                    </h2>
 
-                    <a wire:navigate class="inline-block mt-3 transition duration-200 text-blue-800 dark:text-blue-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline" href="{{ route('blog.post.detail', ['id' => $post->id, 'slug' => $post->slug]) }}" title="Click me to read more">Read more</a>
-                @else
-                    <div class="mt-3 indent-2 text-justify text-gray-800 dark:text-gray-200">{!! Markdown::parse($post->description) !!}</div>
-                @endif
-            </div>
-        </article>
-    @empty
-        <p class="text-red-600 dark:text-red-300">{{ $is_filtered ? 'Your search returned no results.' : 'You have no posts!' }}</p>
-    @endforelse
+                    @if (str_word_count($post->description) > 80)
+                        <div class="mt-3 indent-3 text-justify line-clamp-5 text-gray-800 dark:text-gray-200">{!! Markdown::parse($post->description) !!}</div>
+
+                        <a wire:navigate class="inline-block mt-3 transition duration-200 text-blue-800 dark:text-blue-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline" href="{{ route('blog.post.detail', ['id' => $post->id, 'slug' => $post->slug]) }}" title="Click me to read more">Read more</a>
+                    @else
+                        <div class="mt-3 indent-2 text-justify text-gray-800 dark:text-gray-200">{!! Markdown::parse($post->description) !!}</div>
+                    @endif
+                </div>
+            </article>
+        @empty
+            <p class="text-red-600 dark:text-red-300">{{ $is_filtered ? 'Your search returned no results.' : 'You have no posts!' }}</p>
+        @endforelse
+    </div>
 
     {{ $posts->withQueryString()->links(data: ['scrollTo' => false]) }}
 </div>
