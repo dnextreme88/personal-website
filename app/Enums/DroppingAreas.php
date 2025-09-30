14de42ae0027c365b0deb94bca5539ee5cc9fa6e
Kevin Decena <storm_iz_here@yahoo.com>
2025-09-30 23:05:21 +0800

    update seeders

--- app/Enums/DroppingAreas.php
+++ app/Enums/DroppingAreas.php
@@ -8,6 +8,7 @@ enum DroppingAreas: string implements HasLabel
 {
     case GP_ARCADE_STALL_4 = 'GP Arcade Stall 4';
     case GP_ARCADE_STALL_6 = 'GP Arcade Stall 6';
+    case MABINI_SHOPPING_CENTER_ROOM_204 = 'Mabini Shopping Center Room 204';
     case MABINI_SHOPPING_CENTER_ROOM_209 = 'Mabini Shopping Center Room 209';
     case MABINI_SHOPPING_CENTER_ROOM_212 = 'Mabini Shopping Center Room 212';
     case OLYMPIAN_BUILDING_ROOM_211 = 'Olympian Building Room 211';
@@ -18,6 +19,7 @@ enum DroppingAreas: string implements HasLabel
         return match ($this) {
             self::GP_ARCADE_STALL_4 => 'GP Arcade Stall 4',
             self::GP_ARCADE_STALL_6 => 'GP Arcade Stall 6',
+            self::MABINI_SHOPPING_CENTER_ROOM_204 => 'Mabini Shopping Center Room 204',
             self::MABINI_SHOPPING_CENTER_ROOM_209 => 'Mabini Shopping Center Room 209',
             self::MABINI_SHOPPING_CENTER_ROOM_212 => 'Mabini Shopping Center Room 212',
             self::OLYMPIAN_BUILDING_ROOM_211 => 'Olympian Building Room 211',
